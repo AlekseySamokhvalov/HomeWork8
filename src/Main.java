b6a0db4884f3fc645c2c4fcf9ef94a2bf15d5ef9
AlekseySamokhvalov <6560@mail.ru>
2023-06-20 09:44:01 +0300

    Решение задачи 1.

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,12 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
+        int[] array1 = new int[3];
+        array1[0] = 1;
+        array1[1] = 2;
+        array1[2] = 3;
+        double[] array2 = {1.52, 7.654, 9.986};
+        String[] fruits = {"яблоко", "банан", "апельсин", "виноград", "киви"};
 
     }
 
